86e8f51239031c44741cd23cbe853f684aa991e5
Christina Nuncio <nuncio.christina01@gmail.com>
2021-07-06 12:45:57 -0500

    JS with HTML pt 1 EX 1-7

---
+++ js/external.js
@@ -0,0 +1,26 @@
+// "use strict"
+// // alert("Hi, I am a JavaScript alert!")
+//
+// // var userDecision = confirm("are you sure you don't want to buy stuff on sale?")
+// // console.log(userDecision)
+// //
+// // if (userDecision === true){
+// //     alert("Wise choice!")
+// // } else {
+// //     alert("On no, youre loss!")
+// // }
+//
+// // prompt
+//
+// var currentSpeedInMph = 65
+// var plannedSpeedInMph = prompt("How fast do you plan to go in mph?")
+// console.log(plannedSpeedInMph)
+//
+// var distanceLeftInMiles = prompt("How many miles away are you from your destination?")
+// var timeToDestinationAtCurrentSpeed = (distanceLeftInMiles/currentSpeedInMph) * 60
+// var timeToDestinationAtPlannedSpeedInMph = (distanceLeftInMiles/plannedSpeedInMph) * 60
+// var timeGoing = timeToDestinationAtCurrentSpeed - timeToDestinationAtPlannedSpeedinMph
+// console.log("If you drive at " + plannedSpeedInMph + " mph, you will  arrive " + timeGoing.tofixed(2) + " minutes earlier")
+
+console.log("Hello from external JavaScript")
+
