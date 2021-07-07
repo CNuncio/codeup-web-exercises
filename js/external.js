d2704f15d1e92b756d304a227e464f5fd4723e57
Christina Nuncio <nuncio.christina01@gmail.com>
2021-07-06 23:51:07 -0500

    Reworked solution for JS with HTML 1-3

--- js/external.js
+++ js/external.js
@@ -23,4 +23,41 @@
 // console.log("If you drive at " + plannedSpeedInMph + " mph, you will  arrive " + timeGoing.tofixed(2) + " minutes earlier")
 
 console.log("Hello from external JavaScript")
+alert('Welcome to my Website!')
+var favoriteColor = prompt("What's your favorite color?")
+var favoriteColorToo = alert("Great," + favoriteColor + " is my favorite color too!")
+
+var daysOfRentforLitMer = prompt("How many days did you rent The Little Mermaid?")
+var daysOfRentforBroBear = prompt("How many days did you rent Brother Bear?")
+var daysOfRentforHercules = prompt("How many days did you rent Hercules?")
+var rentalPayDayDollars = prompt("What is the daily rental rate in dollars?")
+
+var totalRentCostOfMovies = (daysOfRentforLitMer + daysOfRentforBroBear + daysOfRentforHercules) * rentalPayDayDollars
+alert("$" + totalRentCostOfMovies)
+
+var $PerHrGoogle = prompt("How much does Google pay per hour?")
+var $PerHrAmazon = prompt("How much does Amazon pay per hour?")
+var $PerHrFaceBook = prompt("How much does FaceBook pay per hour?")
+var GoogleHours = prompt("How many hours this week at Google?")
+var AmazonHours = prompt( "How many hours this week at Amazon?")
+var FaceBookHours = prompt("How many hours this week at FaceBook?")
+var totalPayPerWeek = GoogleHours * $PerHrGoogle + AmazonHours * $PerHrAmazon + FaceBookHours * $PerHrFaceBook
+alert("Total payment is $" + totalPayPerWeek.toFixed(2
+
+    var classIsNotFull = confirm("Confirm that the class is not full")
+    var classScheduleCheck = confirm("Confirm that the schedules check")
+    var studentEnrolled = classIsNotFull && classScheduleCheck
+    alert("Student enrollment status:" + studentEnrolled)
+
+var discountBreakPoint = 2
+var numberOfItems = prompt("How many items were bought?")
+var offerIsNotExpired = confirm("Confirm offer has not expired.")
+var isPremiumNumber = confirm("Comsumer is a Premium Member?")
+var productDiscountIsApplied = offerIsNotExpired && (isPremiumNumber || numberOfItems >= discountBreakPoint)
+alert("Product discount applied:" + productDiscountIsApplied)
+
+
+
+
+
 
