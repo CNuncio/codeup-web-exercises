c4cb930fc45772c8c603bb7a352128b6b775200d
Christina Nuncio <nuncio.christina01@gmail.com>
2021-07-14 16:15:33 -0500

    Loops exercises

---
+++ js/loops_lecture.js
@@ -0,0 +1,31 @@
+var counter = 1000;
+while (counter >= 0){
+    console.log("What time is it? ITS BREAK TIME!!!" + counter)
+    counter--
+}
+
+//
+// var counter = 1000;
+// while (counter >= 0){
+//     console.log("Dormammu's worst nightmare:")
+//     console.log("DORMAMMU! I'VE COME TO BARGAIN!" + counter)
+//     counter--
+// }
+
+//do NOT increment (++) bc will create infinite loop and destroy the multiverse of madness!! DX
+
+//sentinal value is arbitrary value to use to allow user to break out of a loop
+
+// infinite loop maybe too risky to run:
+
+// while (true) alert("MAKE IT STOP!!!!")
+
+// break option conditional to stop the program
+
+// if else example  with break in combination with while true
+
+// while (true) {
+//
+// }
+
+//accumulator variable: var total
