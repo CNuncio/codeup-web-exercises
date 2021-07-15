15132d14e696e7e571be6e7a41692948d46e6dd8
Christina Nuncio <nuncio.christina01@gmail.com>
2021-07-15 10:20:26 -0500

    complete while loops exercise

--- js/while.js
+++ js/while.js
@@ -26,13 +26,15 @@
 //         number =  number * 2
 // }
 
+// do this order with while fist then console.log then increment so it will come up with var number is how it starts
+
 // An ice cream seller can't go home until she sells all of her cones. First write enough code that generates a random number between 50 and 100 representing the amount of cones to sell before you start your loop. Inside of the loop your code should generate another random number between 1 and 5, simulating the amount of cones being bought by her clients. Use a do-while loop to log to the console the amount of cones sold to each person. This is a way get the random numbers for this exercise.
 
 
 // This is how you get a random number between 50 and 100
 // var allCones = Math.floor(Math.random() * 50) + 50;
 // This expression will generate a random number between 1 and 5
-Math.floor(Math.random() * 5) + 1;
+// Math.floor(Math.random() * 5) + 1;
 // The output should be similar to the following:
 //
 //     5 cones sold...  // if there are enough cones
@@ -40,19 +42,16 @@ Math.floor(Math.random() * 5) + 1;
 // Yay! I sold them all! // If there are no more cones
 var allCones = Math.floor(Math.random() * 50) + 50;
 
-alert("You have " + allCones + "cones to start with.")
-{
     do {
-        var howManyCones = Math.floor(Math.random() * 5) + 1;
-        var totalConesLeft = allCones - howManyCones
-        allCones = allCones - howManyCones
-        alert("Number of cones you want to buy " + howManyCones)
-        alert("There are " + totalConesLeft + "cones left to sell.")
-    }
-    while (totalConesLeft >= 0)
-    {
-        alert("We are all out of cones. Sorry!")
-    }
+        var conesBeingBought = Math.floor(Math.random() * 5) + 1;
+        if (conesBeingBought > allCones){
+            console.log("Cannot sell you " + conesBeingBought + " I only have " + allCones)
+        } else {
+            allCones = allCones - conesBeingBought
+            console.log(conesBeingBought + "cones sold  " + allCones + " cones left")
+        }
+    } while (allCones > 0)
+console.log("Yay I sold them all!")
 
 
 
