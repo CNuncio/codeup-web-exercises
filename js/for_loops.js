d4a1664596d8776350e554b9718edccb51be70cf
Christina Nuncio <nuncio.christina01@gmail.com>
2021-07-15 23:19:29 -0500

    loop exercises

--- js/for_loops.js
+++ js/for_loops.js
@@ -32,14 +32,14 @@
 // 199 is odd
 // ...
 
-    for ( count = 0; count < 10, count++){
-var randomNumber = Math.floor(Math.random() * 181 ) * 20;
-if (randomNum 5 2 === 0) {
-console.log(randomNum + " is even")
-} else {
-    console.log(randomNum + "is odd")
-            }
-        }
+//     for ( count = 0; count < 10, count++){
+// var randomNumber = Math.floor(Math.random() * 181 ) * 20;
+// if (randomNum 5 2 === 0) {
+// console.log(randomNum + " is even")
+// } else {
+//     console.log(randomNum + "is odd")
+//             }
+//         }
 
 
 
@@ -56,48 +56,78 @@ console.log(randomNum + " is even")
 // 88888888
 // 999999999
 
-function numberPyramid(num) {
-    for(var count = 1; count <= num; count++) {
-        var stringToDisplay = "";
-    for(var duration = 0; duration < count; duration++) {
- stringToDisplay += count.toString(); }
-    console.log(stringToDisplay);
-}
-    } numberPyramid(9);
+// function numberPyramid(num) {
+//     for(var count = 1; count <= num; count++) {
+//         var stringToDisplay = "";
+//     for(var duration = 0; duration < count; duration++) {
+//  stringToDisplay += count.toString(); }
+//     console.log(stringToDisplay);
+// }
+//     } numberPyramid(9);
 
 // or
 
 
-for (count = 1, count < 10, count++ )
-    var number = count
-var count = 0
-var outputString = ''
-while (count < number) {
-        outputString = outputString + number.toString()
-    count = count + 1
-}
-console.log(outputString)
+// for (count = 1, count < 10, count++ )
+//     var number = count
+// var count = 0
+// var outputString = ''
+// while (count < number) {
+//         outputString = outputString + number.toString()
+//     count = count + 1
+// }
+// console.log(outputString)
 
 // or
 
-for (var outer = 1; outer < 10, outer++){
-    var output = ''
-    for(var inner = 1; inner <= outer; inner++){
-        outer += outer
-    }
-    console.log(output)
-}
+// for (var outer = 1; outer < 10, outer++){
+//     var output = ''
+//     for(var inner = 1; inner <= outer; inner++){
+//         outer += outer
+//     }
+//     console.log(output)
+// }
 
 // or
 
-var crazyOne = 1
-for (count = 1; count < 10; count++) {
-    console.log(count + crazyOne)
-    crazyOne = crazyOne.toString() + 1
+// var crazyOne = 1
+// for (count = 1; count < 10; count++) {
+//     console.log(count + crazyOne)
+//     crazyOne = crazyOne.toString() + 1
+// }
+
+// 5. Create a for loop that uses console.log to create the output shown below.
+//
+//
+// 100
+// 95
+// 90
+// 85
+// 80
+// 75
+// 70
+// 65
+// 60
+// 55
+// 50
+// 45
+// 40
+// 35
+// 30
+// 25
+// 20
+// 15
+// 10
+// 5
+
+// ex from lecture:
+// var count = 100;
+// while (count >=0){
+//     console.log(count);
+//     count = count -1;
+// }
+
+for (count = 100; count >=5; count - 5){
+    console.log(count);
 }
-
-// or
-
-
-
 
