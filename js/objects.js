e9c08cab30c43be019815af93ad02db1c0706745
Christina Nuncio <nuncio.christina01@gmail.com>
2021-07-21 11:10:00 -0500

    objects exercises

--- js/objects.js
+++ js/objects.js
@@ -8,16 +8,16 @@
     //  * with your first and last name. Store this object in a variable named
     //  * `person`.
 
-     var person =
-         { firstName: "Christina",
-        lastName: "Nuncio"
-        }
+    // var person =
+    //     { firstName: "Christina",
+    //    lastName: "Nuncio"
+    //    }
 // console.log(person.firstName)
 //     console.log(person.lastName)
-     // * Example:
-     // *  > console.log(person.firstName) // "Rick"
-     // *  > console.log(person.lastName) // "Sanchez"
-     // */
+    // * Example:
+    // *  > console.log(person.firstName) // "Rick"
+    // *  > console.log(person.lastName) // "Sanchez"
+    // */
 
     // /**
     //  * TODO:
@@ -29,9 +29,11 @@
     //  * > console.log(person.sayHello()) // "Hello from Rick Sanchez!"
     //  */
 
-    person.sayHello = 'Hello from ' + person.firstName + ' ' + person.lastName + '!';
-
-    console.log(person.sayHello);
+//     person.sayHello = function() {
+//          return 'Hello from ' + person.firstName + ' ' + person.lastName + '!';
+//     };
+// //or this.firstName/ this.lastName
+//     console.log(person.sayHello());
 
     // /** TODO:
     //  * HEB has an offer for the shoppers that buy products amounting to
@@ -46,20 +48,34 @@
     //  * and console.log the relevant messages for each person
     //  */
 
-    // var shopper = [
-    //     {name: 'Cameron', amount: 180},
-    //     {name: 'Ryan', amount: 250},
-    //     {name: 'George', amount: 320}
-    // ];
-    //
-    // shopper.forEach(function (shopper) {
-    //     if(shopper.amount >200) {
-    //         console.log("Congrats " + shopper.name + " ! You have spent over $200! Your current total is $" + shopper.amount + " . You now get a 12% discount. Your new discounted total is: " + (shopper.amount - (shopper.amount * .12)) + " ." )
-    //     } else {
-    //         console.log("Sorry " + shopper.name + " , you did not spend over $200. You only spent $" + shopper.amount)
-    //     }
-    // })
+    var shopper = [
+        {name: 'Cameron', amount: 180},
+        {name: 'Ryan', amount: 250},
+        {name: 'George', amount: 320}
+    ];
 
+    shopper.forEach(function (shopper) {
+        if (shopper.amount > 200) {
+            console.log("Congrats to: " + shopper.name + " ! You have spent over $200! Your current total is $" + shopper.amount + " . You will now get a 12% discount! Your new discounted total is: " + (shopper.amount - (shopper.amount * .12)) + " .")
+        } else {
+            console.log("Sorry " + shopper.name + " , you did not spend over $200. You only spent $" + shopper.amount)
+        }
+    })
+
+    //w/o forEach loop :
+    //function calculateDiscount(amount, threshold, discountPercentage) {
+    //if (amount <= threshold) {
+    // return 0
+// } else {
+//     return (amount * discountPercentage))
+    // console.log(calculateDiscount(180, discountThreshold, discountPercentage), 0)
+    // console.log(calculateDiscount(250, discountThreshold, discountPercentage), 30)
+    // console.log(calculateDiscount(3200, discountThreshold, discountPercentage), 38.4)
+
+
+    //var cameronDiscountedAmount = calculatedDiscount(shoppers[0].amount, discountThreshold, discountPercentage)
+    // var output1 = "Cameron is purchasing " + shoppers[0].amount ' of things. He will get " + cameronDiscountedAmeount + " off the purchase and pay " + (shoppers[0].amount - cameronDiscountedAmount) + ".";
+// console.log(output1)
     // /** TODO:
     //  * Create an array of objects that represent books and store it in a
     //  * variable named `books`. Each object should have a title and an author
