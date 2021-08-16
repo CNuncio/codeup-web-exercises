a732eea3effc76cb45ad6fc81ef2d7ffe154a2d5
Christina Nuncio <nuncio.christina01@gmail.com>
2021-08-16 10:01:24 -0500

    dom warmUps

---
+++ js/dom-warmups.js
@@ -0,0 +1,7 @@
+// "use strict";
+//
+// var buttonOne = document.getElementById('afternoon');
+//
+// buttonOne.addEventListener('click', function () {
+//     return mainHeader.innerHTML = 'Good Afternoon, Draco';
+// });
